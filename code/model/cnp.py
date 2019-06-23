d97a74aa358d749b9c245ea48d49b933e6f536e6
Daniel Dubnikov <dadubnik@microsoft.com>
2019-06-23 16:04:41 +0300

    model files

---
+++ code/model/cnp.py
@@ -0,0 +1,38 @@
+import torch
+import torch.nn as nn
+import torchvision
+from torch.autograd import Variable
+from torch.utils.data import Dataset, DataLoader
+from encoder import Encoder
+from decoder import Decoder
+from aggregator import AttentionAggregator, AverageAggregator
+
+
+class CNP(nn.Module):
+  def __init__(self, context_size, target_size, hidden_repr, enc_hidden_layers, dec_hidden_layers, output_size, attn=False, to_cuda=False):
+    super(CNP, self).__init__()
+    self.encoder = Encoder(context_size, enc_hidden_layers, hidden_repr, to_cuda)
+    if attn:
+      self.aggregator = AttentionAggregator(hidden_repr, to_cuda)
+    else:
+      self.aggregator = AverageAggregator(hidden_repr, to_cuda)
+    self.decoder = Decoder(hidden_repr, target_size, dec_hidden_layers, output_size, to_cuda)
+    self.representation = None
+    if to_cuda:
+      self.encoder = self.encoder.cuda()
+      self.aggregator = self.aggregator.cuda()
+      self.decoder = self.decoder.cuda()
+
+  def forward(self, context, target):
+    encodings = self.encoder(context)
+    self.representation = self.aggregator(encodings)
+    
+    x = self.concat_repr_to_target(target)
+    
+    predictions = self.decoder(x)
+    return predictions
+  
+  def concat_repr_to_target(self, target):
+    x = self.representation.repeat(target.shape[0], 1)
+    x = torch.cat((x, target), dim=1)
+    return x
---
+++ code/model/decoder.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Decoder(nn.Module):
+  def __init__(self, agg_size, input_size, hidden_layers, output_size, to_cuda=False):
+    super(Decoder, self).__init__()
+    self.fcs = []
+    inp = agg_size + input_size
+    for hidden_layer in hidden_layers:
+      self.fcs.append(nn.Linear(inp, hidden_layer))
+      inp = hidden_layer
+    self.output_fc = nn.Linear(inp, output_size)
+    
+    if to_cuda:
+      for i in range(len(self.fcs)):
+        self.fcs[i] = self.fcs[i].cuda()
+      self.output_fc = self.output_fc.cuda()
+
+  def forward(self, x):
+    for fc in self.fcs:
+      x = fc(x)
+      x = F.relu(x)
+    x = self.output_fc(x)
+    return x
