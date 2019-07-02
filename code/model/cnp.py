f8e7d73034c5184ec754697e9306cc8fc445a090
Daniel Dubnikov <dadubnik@microsoft.com>
2019-07-02 14:54:59 +0300

    prepare model for traininig

--- code/model/cnp.py
+++ code/model/cnp.py
@@ -7,30 +7,45 @@ from aggregator import AttentionAggregator, AverageAggregator
 
 
 class CNP(nn.Module):
-  def __init__(self, context_size, target_size, hidden_repr, enc_hidden_layers, dec_hidden_layers, output_size, attn=False, to_cuda=False):
-    super(CNP, self).__init__()
-    self.encoder = Encoder(context_size, enc_hidden_layers, hidden_repr, to_cuda)
-    if attn:
-      self.aggregator = AttentionAggregator(hidden_repr, to_cuda)
-    else:
-      self.aggregator = AverageAggregator(hidden_repr, to_cuda)
-    self.decoder = Decoder(hidden_repr, target_size, dec_hidden_layers, output_size, to_cuda)
-    self.representation = None
-    if to_cuda:
-      self.encoder = self.encoder.cuda()
-      self.aggregator = self.aggregator.cuda()
-      self.decoder = self.decoder.cuda()
+    def __init__(self, context_size, target_size, hidden_repr, enc_hidden_layers, dec_hidden_layers, output_size, attn=False, to_cuda=False):
+        super(CNP, self).__init__()
+        self.encoder = Encoder(
+            context_size, enc_hidden_layers, hidden_repr, to_cuda)
+        if attn:
+            self.aggregator = AttentionAggregator(hidden_repr, to_cuda)
+        else:
+            self.aggregator = AverageAggregator(hidden_repr, to_cuda)
+        self.decoder = Decoder(hidden_repr, target_size,
+                               dec_hidden_layers, output_size, to_cuda)
+        self.representation = None
+        if to_cuda:
+            self.encoder = self.encoder.cuda()
+            self.aggregator = self.aggregator.cuda()
+            self.decoder = self.decoder.cuda()
 
-  def forward(self, context, target):
-    encodings = self.encoder(context)
-    self.representation = self.aggregator(encodings)
-    
-    x = self.concat_repr_to_target(target)
-    
-    predictions = self.decoder(x)
-    return predictions
-  
-  def concat_repr_to_target(self, target):
-    x = self.representation.repeat(target.shape[0], 1)
-    x = torch.cat((x, target), dim=1)
-    return x
+    def forward(self, context, target):
+        encodings = self.encoder(context)
+        self.representation = self.aggregator(encodings)
+
+        x = self.concat_repr_to_target(target)
+
+        predictions = self.decoder(x)
+        return predictions
+
+    def concat_repr_to_target(self, target):
+        x = self.representation.repeat(target.shape[0], 1)
+        x = torch.cat((x, target), dim=1)
+        return x
+
+
+    def eval_model(self):
+        self.eval()
+        self.encoder.eval()
+        self.decoder.eval()
+        self.aggregator.eval()
+
+    def train_model(self):
+        self.train()
+        self.encoder.train()
+        self.decoder.train()
+        self.aggregator.train()
--- code/training.py
+++ code/training.py
@@ -6,71 +6,56 @@ from torch.utils.data import Dataset, DataLoader
 
 
 class Trainer():
-    def __init__(self, model, to_cuda):
+    def __init__(self, model, training_dataset, batch_size, learning_rate, epoch_count, to_cuda):
         self.model = model
+        self.training_dataset = training_dataset
+        self.learning_rate = learning_rate
+        self.batch_size = batch_size
+        self.epoch_count = epoch_count
         self.to_cuda = to_cuda
 
     def train(self):
-        loss_function = nn.BCEWithLogitsLoss(reduction='mean')
-        optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+        loss_function = nn.CrossEntropyLoss()
+        optimizer = torch.optim.Adam(model.parameters(), lr=self.learning_rate)
+        train_loader = DataLoader(
+            dataset=self.training_dataset, batch_size=self.batch_size, shuffle=True)
+        train_loss_per_epoch = []
 
-        train_loader = DataLoader(dataset=training_dataset, batch_size=batch_size, shuffle=True)
+        for epoch in range(self.epoch_count):
+            epoch_train_loss = []
 
-        train_loss_history = []
-        test_recalls = []
-        best_recall = float('-inf')
-        for epoch in range(epoch_count):
-        epoch_train_loss = []
-        epoch_train_recall = []
-        
-        # train
-        train_model(model)
-        for target_xs_batch, target_ys_batch in train_loader:
-            target_xs = torch.stack(target_xs_batch, dim=1).float()
-            target_ys = torch.stack(target_ys_batch, dim=1).float()
-            if to_cuda:
-            target_xs = target_xs.cuda()
-            target_ys = target_ys.cuda()
-            
-            target_xs = Variable(target_xs, requires_grad=True)
-            target_ys = Variable(target_ys, requires_grad=True)
+            # train
+            self.model.train_model()
+            for context_batch, target_xs_batch, target_ys_batch in train_loader:
+                context = torch.stack(context_batch, dim=1).float()
+                target_xs = torch.stack(target_xs_batch, dim=1).float()
+                target_ys = torch.stack(target_ys_batch, dim=1).float()
+                if self.to_cuda:
+                    context = context.cuda()
+                    target_xs = target_xs.cuda()
+                    target_ys = target_ys.cuda()
 
-            # feedforward - backprop
-            optimizer.zero_grad()
-            outputs = model(full_context, target_xs)
-            loss = loss_function(outputs, target_ys)
-            loss.backward()
-            optimizer.step()
+                context = Variable(context, requires_grad=True)
+                target_xs = Variable(target_xs, requires_grad=True)
+                target_ys = Variable(target_ys, requires_grad=True)
 
-            # train loss
-            epoch_train_loss.append(loss.item())
-            
-            # train recall
-            train_recall = compute_recall(outputs, target_xs, target_ys)
-            epoch_train_recall.append(train_recall)
-            
-        # test
-        eval_model(model)
+                # feedforward - backprop
+                optimizer.zero_grad()
+                outputs = self.model(context, target_xs)
+                loss = loss_function(outputs, target_ys)
+                loss.backward()
+                optimizer.step()
 
-        outputs = model.decoder(model.concat_repr_to_target(test_xs))
+                # train loss
+                epoch_train_loss.append(loss.item())
 
-        # compute epoch recall
-        recall = compute_recall(outputs, test_xs, test_ys)
-        
-        # save best representation
-        if recall > best_recall:
-            best_recall = recall
-            export_model(model, model.representation, attn, epoch)
-        
-        test_recalls.append(recall)
+            # compute epoch loss
+            cur_train_loss = sum(epoch_train_loss) / len(epoch_train_loss)
+            train_loss_per_epoch.append(cur_train_loss)
 
-        # compute epoch loss
-        cur_train_loss = sum(epoch_train_loss) / len(epoch_train_loss)
-        train_loss_history.append(cur_train_loss)
-        
-        # compute epoch average train recall
-        cur_train_recall = sum(epoch_train_recall) / len(epoch_train_recall)
+            if (epoch) % 10 == 0:
+                print('Epoch [%d/%d] Train Loss: %.4f' %
+                      (epoch+1, self.epoch_count, cur_train_loss))
+                print()
 
-        if (epoch+1) % 10 == 0 or epoch == 0:
-            print('Epoch [%d/%d] Train Loss: %.4f, Train Recall: %.4f, Test Recall: %.4f' %(epoch+1, epoch_count, cur_train_loss, cur_train_recall, recall))
-            print()
+        return train_loss_per_epoch
