ddbfb4a64003fe9d7b82986ce56bb1a128556111
Daniel Dubnikov <dadubnik@microsoft.com>
2019-07-02 14:56:02 +0300

    fix indentation

--- code/model/aggregator.py
+++ code/model/aggregator.py
@@ -4,31 +4,31 @@ import torch.nn.functional as F
 
 
 class AverageAggregator(nn.Module):
-  def __init__(self, hidden_repr, to_cuda = False):
-    super(AverageAggregator, self).__init__()
+    def __init__(self, hidden_repr, to_cuda=False):
+        super(AverageAggregator, self).__init__()
 
-  def forward(self, x):
-    x = torch.mean(x, 0)
-    return x
+    def forward(self, x):
+        x = torch.mean(x, 0)
+        return x
 
 
 class AttentionAggregator(nn.Module):
-  def __init__(self, hidden_repr, to_cuda = False):
-    super(AttentionAggregator, self).__init__()
-    self.fc = nn.Linear(hidden_repr, hidden_repr)
-    self.weight_vec = nn.Parameter(torch.FloatTensor(hidden_repr, 1))
-    torch.nn.init.xavier_normal_(self.weight_vec)
-    
-    if to_cuda:
-      self.fc = self.fc.cuda()
+    def __init__(self, hidden_repr, to_cuda=False):
+        super(AttentionAggregator, self).__init__()
+        self.fc = nn.Linear(hidden_repr, hidden_repr)
+        self.weight_vec = nn.Parameter(torch.FloatTensor(hidden_repr, 1))
+        torch.nn.init.xavier_normal_(self.weight_vec)
 
-  def forward(self, x):
-    energies = self.fc(x)
-    energies = torch.matmul(energies, self.weight_vec)
-    weights = F.softmax(energies, dim=0)
+        if to_cuda:
+            self.fc = self.fc.cuda()
 
-    x = x.permute([1, 0])  # transpose
-    x = torch.matmul(x, weights)
-    x = x.permute([1, 0])  # transpose
+    def forward(self, x):
+        energies = self.fc(x)
+        energies = torch.matmul(energies, self.weight_vec)
+        weights = F.softmax(energies, dim=0)
 
-    return torch.sum(x, dim=0)
+        x = x.permute([1, 0])  # transpose
+        x = torch.matmul(x, weights)
+        x = x.permute([1, 0])  # transpose
+
+        return torch.sum(x, dim=0)
--- code/model/encoder.py
+++ code/model/encoder.py
@@ -4,23 +4,23 @@ import torch.nn.functional as F
 
 
 class Encoder(nn.Module):
-  def __init__(self, input_size, hidden_layers, output_size, to_cuda = False):
-    super(Encoder, self).__init__()
-    self.fcs = []
-    inp = input_size
-    for hidden_layer in hidden_layers:
-      self.fcs.append(nn.Linear(inp, hidden_layer))
-      inp = hidden_layer
-    self.output_fc = nn.Linear(inp, output_size)
-    
-    if to_cuda:
-      for i in range(len(self.fcs)):
-        self.fcs[i] = self.fcs[i].cuda()
-      self.output_fc = self.output_fc.cuda()
+    def __init__(self, input_size, hidden_layers, output_size, to_cuda=False):
+        super(Encoder, self).__init__()
+        self.fcs = []
+        inp = input_size
+        for hidden_layer in hidden_layers:
+            self.fcs.append(nn.Linear(inp, hidden_layer))
+            inp = hidden_layer
+        self.output_fc = nn.Linear(inp, output_size)
 
-  def forward(self, x):
-    for fc in self.fcs:
-      x = fc(x)
-      x = F.relu(x)
-    x = self.output_fc(x)
-    return x
+        if to_cuda:
+            for i in range(len(self.fcs)):
+                self.fcs[i] = self.fcs[i].cuda()
+            self.output_fc = self.output_fc.cuda()
+
+    def forward(self, x):
+        for fc in self.fcs:
+            x = fc(x)
+            x = F.relu(x)
+        x = self.output_fc(x)
+        return x
