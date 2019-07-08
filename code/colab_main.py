5fba96aa3b993e0f171f46ad8c5c864a3f69df51
Uri Shaham <t-urshah@microsoft.com>
2019-07-08 14:16:45 +0300

    Create colab_main.py
    add colab_main.py

---
+++ code/colab_main.py
@@ -0,0 +1,23 @@
+import pickle as cPickle
+import torch
+import logging
+from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
+from data_processing.dataset import text_dataset
+from model.cnp import CNP
+from training import Trainer
+
+
+def read_data(path):
+    with open(path, "r") as f:
+        text = f.readlines()
+    text = [sent.rstrip("\n").split(" ") for sent in text]
+    return text
+
+
+if __name__ == "__main__":
+    sents = read_data("data/APRC/APRC_small_mock.txt")
+    to_cuda = True
+    dataset = text_dataset(sents, to_cuda)
+    model = CNP(769, 1, 800, [700], [700], len(dataset.id2w), dataset.max_seq_len, dataset.max_masked_size)
+    trainer = Trainer(model, dataset, None, 2, 0.001, 100, to_cuda)
+    trainer.run()
