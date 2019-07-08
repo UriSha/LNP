8c83277acac143d9e26d88028d39a4c97a3a70a2
Uri Shaham <t-urshah@microsoft.com>
2019-07-08 14:20:56 +0300

    Update colab_main.py
    2000 epochs

--- code/colab_main.py
+++ code/colab_main.py
@@ -17,7 +17,8 @@ def read_data(path):
 if __name__ == "__main__":
     sents = read_data("data/APRC/APRC_small_mock.txt")
     to_cuda = True
+    n_epoches = 2000
     dataset = text_dataset(sents, to_cuda)
     model = CNP(769, 1, 800, [700], [700], len(dataset.id2w), dataset.max_seq_len, dataset.max_masked_size, to_cuda=to_cuda)
-    trainer = Trainer(model, dataset, None, 2, 0.001, 100, to_cuda)
+    trainer = Trainer(model, dataset, None, 2, 0.001, n_epoches, to_cuda)
     trainer.run()
