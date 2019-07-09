94f7864e11699edadc0241ee0d5d9131a34ed8ce
Daniel Dubnikov <dadubnik@microsoft.com>
2019-07-09 16:51:09 +0300

    Update training.py

--- code/training.py
+++ code/training.py
@@ -109,7 +109,7 @@ class Trainer():
                 cur_eval_loss = sum(epoch_eval_loss) / len(epoch_eval_loss)
                 cur_eval_acc = sum(epoch_eval_acc) / len(epoch_eval_acc)
                 cur_eval_perplexity = np.exp(cur_eval_loss)
-                eval_loss_per_epoch.append(cur_dev_loss)
+                eval_loss_per_epoch.append(cur_eval_loss)
                 eval_perplexity_per_epoch(cur_eval_perplexity)
             else:
                 cur_eval_loss = 0
