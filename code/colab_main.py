f1758b07295c6b2aa39da5a9628203b10dc89180
Daniel Dubnikov <dadubnik@microsoft.com>
2019-07-09 15:00:25 +0300

    Update colab_main.py

--- code/colab_main.py
+++ code/colab_main.py
@@ -1,13 +1,14 @@
-import pickle as cPickle
+import sys
 import torch
+import pickle as cPickle
 import logging
+import argparse
 from pytorch_pretrained_bert import BertTokenizer, BertModel, BertForMaskedLM
 from data_processing.dataset import text_dataset
 from data_processing.dataset_once_random import text_dataset_once_random
+from sklearn.model_selection import train_test_split
 from model.cnp import CNP
 from training import Trainer
-import sys
-import argparse
 
 
 def read_data(path):
@@ -38,16 +39,40 @@ if __name__ == "__main__":
                         help="to_cuda (default: True)",
                         default=True,
                         type=bool)
+    parser.add_argument('-sc', '--sent_count',
+                        help="sent_count (default: no limit)",
+                        default=0,
+                        type=int)
+    parser.add_argument('-bs', '--batch_size',
+                        help="batch_size (default: 16)",
+                        default=16,
+                        type=int)
     args = parser.parse_args()
 
     sents = read_data("data/APRC/{}.txt".format(args.model))
 
-    if args.dataset_random_every_time:
-        dataset = text_dataset(sents, to_cuda=args.to_cuda)
+    if args.sent_count == 0:
+        sent_count = len(sents)
     else:
-        dataset = text_dataset_once_random(sents, to_cuda=args.to_cuda)
+        sent_count = args.sent_count
+    train_sents, eval_sents = train_test_split(
+        sents[:sent_count], test_size=0.1)
+    if args.dataset_random_every_time:
+        train_dataset = text_dataset(train_sents, to_cuda=args.to_cuda)
+    else:
+        train_dataset = text_dataset_once_random(
+            train_sents, to_cuda=args.to_cuda)
 
-    model = CNP(769, 1, 800, [700], [700], len(dataset.id2w), dataset.max_seq_len, dataset.max_masked_size,
+    eval_dataset = text_dataset_once_random(eval_sents, to_cuda=args.to_cuda)
+    model = CNP(context_size=769, 
+                target_size=1, 
+                hidden_repr=800, 
+                enc_hidden_layers=[800, 800], 
+                dec_hidden_layers=[850, 1000], 
+                output_size=len(dataset.id2w), 
+                max_sent_len=dataset.max_seq_len, 
+                max_target_size=dataset.max_masked_size,
                 to_cuda=args.to_cuda)
-    trainer = Trainer(model, dataset, None, 2, args.learning_rate, args.epochs, args.to_cuda)
+    trainer = Trainer(model, train_dataset, eval_dataset, args.batch_size,
+                      args.learning_rate, args.epochs, args.to_cuda)
     trainer.run()
