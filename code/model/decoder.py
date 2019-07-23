7d283efeca4ccb1d3c57936281caaf07b5381bc4
uri <urish92@gmail.com>
2019-07-23 15:44:06 +0300

    Add APRC data

--- code/model/decoder.py
+++ code/model/decoder.py
@@ -1,12 +1,12 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class Decoder(nn.Module):
-    def __init__(self, hidden_repr, input_size, hidden_layers, output_size, to_cuda=False):
+    def __init__(self, hidden_repr, input_size, hidden_layers, output_size, dropout, to_cuda=False):
         super(Decoder, self).__init__()
         self.fcs = []
+        self.dropout = nn.Dropout(dropout)
         inp = hidden_repr + input_size
         for hidden_layer in hidden_layers:
             self.fcs.append(nn.Linear(inp, hidden_layer))
@@ -18,10 +18,10 @@ class Decoder(nn.Module):
                 self.fcs[i] = self.fcs[i].cuda()
             self.output_fc = self.output_fc.cuda()
 
-
     def forward(self, x):
         for fc in self.fcs:
             x = fc(x)
             x = F.relu(x)
+            x = self.dropout(x)
         x = self.output_fc(x)
         return x
--- code/model/encoder.py
+++ code/model/encoder.py
@@ -1,13 +1,13 @@
-import torch
 import torch.nn as nn
 import torch.nn.functional as F
 
 
 class Encoder(nn.Module):
-    def __init__(self, input_size, hidden_layers, output_size, to_cuda=False):
+    def __init__(self, input_size, hidden_layers, output_size, dropout, to_cuda=False):
         super(Encoder, self).__init__()
         self.fcs = []
         inp = input_size
+        self.dropout = nn.Dropout(dropout)
         for hidden_layer in hidden_layers:
             self.fcs.append(nn.Linear(inp, hidden_layer))
             inp = hidden_layer
@@ -18,10 +18,10 @@ class Encoder(nn.Module):
                 self.fcs[i] = self.fcs[i].cuda()
             self.output_fc = self.output_fc.cuda()
 
-
     def forward(self, x):
         for fc in self.fcs:
             x = fc(x)
             x = F.relu(x)
+            x = self.dropout(x)
         x = self.output_fc(x)
         return x
