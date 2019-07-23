3233d1fada3382e25b72dbb5759ebc930f70981c
uri <urish92@gmail.com>
2019-07-23 17:46:10 +0300

    print "Init AbstractTextProcessor"

--- code/model/decoder.py
+++ code/model/decoder.py
@@ -6,22 +6,24 @@ class Decoder(nn.Module):
     def __init__(self, hidden_repr, input_size, hidden_layers, output_size, dropout, to_cuda=False):
         super(Decoder, self).__init__()
         self.fcs = []
-        self.dropout = nn.Dropout(dropout)
+        self.dps = []
         inp = hidden_repr + input_size
         for hidden_layer in hidden_layers:
             self.fcs.append(nn.Linear(inp, hidden_layer))
+            self.dps.append(nn.Dropout(dropout))
             inp = hidden_layer
         self.output_fc = nn.Linear(inp, output_size)
 
         if to_cuda:
             for i in range(len(self.fcs)):
                 self.fcs[i] = self.fcs[i].cuda()
+                self.dps[i] = self.dps[i].cuda()
             self.output_fc = self.output_fc.cuda()
 
     def forward(self, x):
-        for fc in self.fcs:
-            x = fc(x)
+        for i in range(len(self.fcs)):
+            x = self.fcs[i](x)
             x = F.relu(x)
-            x = self.dropout(x)
+            x = self.dps[i](x)
         x = self.output_fc(x)
         return x
--- code/model/encoder.py
+++ code/model/encoder.py
@@ -6,22 +6,25 @@ class Encoder(nn.Module):
     def __init__(self, input_size, hidden_layers, output_size, dropout, to_cuda=False):
         super(Encoder, self).__init__()
         self.fcs = []
+        self.dps = []
         inp = input_size
         self.dropout = nn.Dropout(dropout)
         for hidden_layer in hidden_layers:
             self.fcs.append(nn.Linear(inp, hidden_layer))
+            self.dps.append(nn.Dropout(dropout))
             inp = hidden_layer
         self.output_fc = nn.Linear(inp, output_size)
 
         if to_cuda:
             for i in range(len(self.fcs)):
                 self.fcs[i] = self.fcs[i].cuda()
+                self.dps[i] = self.dps[i].cuda()
             self.output_fc = self.output_fc.cuda()
 
     def forward(self, x):
-        for fc in self.fcs:
-            x = fc(x)
+        for i in range(len(self.fcs)):
+            x = self.fcs[i](x)
             x = F.relu(x)
-            x = self.dropout(x)
+            x = self.dps[i](x)
         x = self.output_fc(x)
         return x
