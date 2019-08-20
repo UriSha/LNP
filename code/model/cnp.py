a9d873efbdafead7f786b63b8df4673a0c02026b
omerk1 <omerk1@mail.tau.ac.il>
2019-08-20 15:51:03 +0300

    stochastic latent encoder- not tested at all

--- code/model/cnp.py
+++ code/model/cnp.py
@@ -1,18 +1,20 @@
 import math
-import torch
-import torch.nn as nn
+
 from sklearn.preprocessing import normalize
-from model.encoder import Encoder
+
 from model.aggregator import AttentionAggregator, AverageAggregator
-from model.decoder import Decoder
-from model.self_attention_encoder import SelfAttentionEncoderLayer
 from model.cross_attention_aggregator import CrossAttentionAggregator
+from model.decoder import Decoder
+from model.encoder import Encoder
+from model.latent_encoder import LatentEncoder
+
 from model.transformer import *
 
 
 class CNP(nn.Module):
     def __init__(self, embedding_size, hidden_repr, enc_hidden_layers, dec_hidden_layers, max_target_size, w2id,
-                 id2w, emb_weight, padding_idx, max_seq_len, use_weight_matrix, nheads=2, use_pos_embedding=True, dropout=0.1, attn=False, concat_embeddings=False, normalize_weights=True, to_cuda=False):
+                 id2w, emb_weight, padding_idx, max_seq_len, use_weight_matrix, nheads=2, use_pos_embedding=True,
+                 dropout=0.1, attn=False, concat_embeddings=False, normalize_weights=True, to_cuda=False):
         super(CNP, self).__init__()
         self.use_pos_embedding = use_pos_embedding
         self.attn = attn
@@ -39,11 +41,22 @@ class CNP(nn.Module):
 
             # self.encoder = Transformer(input_size, nhead=nheads, num_encoder_layers=len(enc_hidden_layers), num_decoder_layers=len(enc_hidden_layers), dim_feedforward=enc_hidden_layers[0], dropout=dropout)
             # self.aggregator = None
-            self.encoder = TransformerEncoder(TransformerEncoderLayer(input_size, nhead=nheads, dim_feedforward=enc_hidden_layers[0], dropout=dropout), num_layers=len(enc_hidden_layers))
+            self.encoder = TransformerEncoder(
+                TransformerEncoderLayer(input_size, nhead=nheads, dim_feedforward=enc_hidden_layers[0],
+                                        dropout=dropout), num_layers=len(enc_hidden_layers))
             self.aggregator = CrossAttentionAggregator(embedding_size, nheads, dropout, to_cuda)
-            self.latent_encoder = TransformerEncoder(TransformerEncoderLayer(input_size, nhead=nheads, dim_feedforward=enc_hidden_layers[0], dropout=dropout), num_layers=len(enc_hidden_layers))
-            self.latent_aggregator = AverageAggregator()
-            
+
+            self.latent_encoder = LatentEncoder(d_model=input_size,
+                                                nhead=nheads,
+                                                num_hidden=len(enc_hidden_layers),
+                                                num_latent=len(enc_hidden_layers),
+                                                dim_feedforward=enc_hidden_layers[0],
+                                                dropout=dropout)
+            # self.latent_encoder = TransformerEncoder(
+            #     TransformerEncoderLayer(input_size, nhead=nheads, dim_feedforward=enc_hidden_layers[0],
+            #                             dropout=dropout), num_layers=len(enc_hidden_layers))
+            # self.latent_aggregator = AverageAggregator()
+
             self.decoder = Decoder(input_size + embedding_size, dec_hidden_layers, output_size, dropout, to_cuda)
         else:
             self.encoder = Encoder(input_size, enc_hidden_layers, hidden_repr, dropout, to_cuda)
@@ -79,14 +92,18 @@ class CNP(nn.Module):
         if to_cuda:
             if self.embedding_matrix is not None:
                 self.embedding_matrix = self.embedding_matrix.cuda()
+
             self.embedding = self.embedding.cuda()
             self.encoder = self.encoder.cuda()
             if self.aggregator is not None:
                 self.aggregator = self.aggregator.cuda()
+
             if self.latent_encoder is not None:
                 self.latent_encoder = self.latent_encoder.cuda()
+
             if self.latent_aggregator is not None:
                 self.latent_aggregator = self.latent_aggregator.cuda()
+
             self.decoder = self.decoder.cuda()
             self.pos_embeddings = self.pos_embeddings.cuda()
 
@@ -96,7 +113,7 @@ class CNP(nn.Module):
             pos_embeddings = self.pos_embeddings(context_pos)
         else:
             pos_embeddings = context_pos.unsqueeze(dim=2).float()
-        
+
         if self.concat_embeddings:
             context = torch.cat((sent_embeddings, pos_embeddings), dim=2)
         else:
@@ -111,11 +128,26 @@ class CNP(nn.Module):
             context = context.transpose(0, 1)
             encodings = self.encoder(context, src_key_padding_mask=context_mask)
             encodings = encodings.transpose(0, 1)
-            representations = self.aggregator(q=emb_target, k=pos_embeddings, r=encodings, context_mask=context_mask, target_mask=target_mask)
+            representations = self.aggregator(q=emb_target, k=pos_embeddings, r=encodings, context_mask=context_mask,
+                                              target_mask=target_mask)
 
-            latent_encodings = self.latent_encoder(context, src_key_padding_mask=context_mask)
-            latent_encodings = latent_encodings.transpose(0, 1)
-            latent_representations = self.latent_aggregator(latent_encodings, context_mask)
+            # latent path- new
+            prior_mu, prior_var, prior = self.latent_encoder(context, context_mask)
+
+            train = 1
+            # For training
+            if train is not None:       # TODO- check how to change it
+                posterior_mu, posterior_var, posterior = self.latent_encoder(emb_target, target)
+                z = posterior
+
+            # For Generation
+            else:
+                z = prior
+
+            # latent path- old
+            # latent_encodings = self.latent_encoder(context, src_key_padding_mask=context_mask)
+            # latent_encodings = latent_encodings.transpose(0, 1)
+            # latent_representations = self.latent_aggregator(latent_encodings, context_mask)
 
             # representations = self.encoder(context.transpose(0, 1), emb_target.transpose(0, 1), src_key_padding_mask=context_mask, tgt_key_padding_mask=target_mask)
             # representations = representations.transpose(0, 1)
@@ -124,8 +156,8 @@ class CNP(nn.Module):
                 x = torch.cat((representations, emb_target), dim=2)
             else:
                 x = representations + emb_target
-            
-            latent_representations = torch.repeat_interleave(latent_representations, self.max_target_size, dim=1)
+
+            latent_representations = torch.repeat_interleave(z, self.max_target_size, dim=1)
             x = torch.cat((x, latent_representations), dim=2)
         else:
             encodings = self.encoder(context, context_mask)
@@ -136,7 +168,7 @@ class CNP(nn.Module):
 
         if self.embedding_matrix is not None:
             predicted_embeddings = torch.matmul(predicted_embeddings, self.embedding_matrix)
-        
+
         return predicted_embeddings
 
     def create_pos_embeddings_matrix(self, max_seq_len, embed_size):
--- code/model/latent_encoder.py
+++ code/model/latent_encoder.py
@@ -1,10 +1,11 @@
-from transformer import *
 from aggregator import AverageAggregator
+from transformer import *
+
 
 class LatentEncoder(torch.nn.Module):
     """The Latent Encoder."""
 
-    def __init__(self, d_model, num_latents, nhead, dim_feedforward=2048, dropout=0.1):
+    def __init__(self, d_model, num_hidden, num_latent, nhead, dim_feedforward=2048, dropout=0.1):
         """(A)NP latent encoder.
 
         Args:
@@ -21,13 +22,26 @@ class LatentEncoder(torch.nn.Module):
                                                   dropout=dropout),
             num_layers=6
         )
-        self._num_latents = num_latents
+        self._num_latent = num_latent
         self.aggregator = AverageAggregator()
+
+        self.mu = Linear(num_hidden, num_latent)
+        self.log_sigma = Linear(num_hidden, num_latent)
 
     def forward(self, src, src_mask=None, src_key_padding_mask=None):
         latent_representations = self.encoder(src=src, mask=src_mask, src_key_padding_mask=src_key_padding_mask)
 
         latent_aggregated_representation = self.aggregator(x=latent_representations, x_mask=src_mask)
 
-        return latent_aggregated_representation
+        # get mu and sigma
+        mu = self.mu(latent_aggregated_representation)
+        log_sigma = self.log_sigma(latent_aggregated_representation)
+
+        # reparameterization trick
+        std = torch.exp(0.5 * log_sigma)
+        eps = torch.randn_like(std)
+        z = eps.mul(std).add_(mu)
+
+        # return distribution
+        return mu, log_sigma, z
 
