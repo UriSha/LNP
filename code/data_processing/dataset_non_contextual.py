34084158409315717d05bc232e7bdb498ad3e515
Daniel Dubnikov <duvni@users.noreply.github.com>
2019-08-24 15:36:00 +0300

    Update dataset_non_contextual.py

--- code/data_processing/dataset_non_contextual.py
+++ code/data_processing/dataset_non_contextual.py
@@ -1,13 +1,7 @@
 import math
-import random
-
 import torch
-# from pytorch_pretrained_bert import BertTokenizer
+import random
 from torch.utils.data import Dataset
-
-
-# from pytorch_pretrained_bert import BertModel
-
 
 class DatasetNonContextual(Dataset):
     def __init__(self, text_as_list, w2id, id2w, max_seq_len, mask_ratios, transform=None,
@@ -23,34 +17,7 @@ class DatasetNonContextual(Dataset):
         self.MASK_SYMBOL = '<MASK>'
         self.mem = {}
         self.current_mask_ratio_index = 0
-        # self.data = self.create_data(text_as_list, tokenizer)
 
-    # def create_data(self, text_as_list, tokenizer):
-    #     bert_pretrained = BertModel.from_pretrained('bert-base-uncased')
-    #     bert_pretrained.eval()
-    #
-    #     if self.to_cuda:
-    #         bert_pretrained.to('cuda')
-    #
-    #     num_of_sents = len(text_as_list)
-    #     one_tenth = num_of_sents / 10
-    #     cur_count = 0
-    #     sents_processed = 0
-    #
-    #     data = []
-    #     for original_sent in text_as_list:
-    #         data_instance = self.generate_data_instance_fron_sentence(original_sent=original_sent,
-    #                                                                   tokenizer=tokenizer,
-    #                                                                   bert_pretrained=bert_pretrained)
-    #         sents_processed += 1
-    #         cur_count += 1
-    #         data.append(data_instance)
-    #
-    #         if cur_count == one_tenth:
-    #             print("Processed {} out of {} data_instances".format(sents_processed, num_of_sents))
-    #             cur_count = 0
-    #
-    #     return data
 
     def __getitem__(self, index):
         if index in self.mem:
@@ -80,67 +47,12 @@ class DatasetNonContextual(Dataset):
         self.mem[index] = padded_sent_ids_tensor, anti_mask_indices, paddings_mask, target_xs, target_xs_mask, target_ys
         return self.mem[index]
 
-    # def generate_data_instance_fron_sentence(self, original_sent, tokenizer, bert_pretrained):
-    #     sentence = original_sent.copy()
-    #
-    #     sent, masked_indices, target_xs, target_ys = self.mask_sent(sentence)
-    #     # print("masked sentance: ", sentence)
-    #     sent.insert(0, "[CLS]")
-    #     sent.append("[SEP]")
-    #
-    #     sent = " ".join(sent)
-    #
-    #     # Tokenized input
-    #     tokenized_sent = tokenizer.tokenize(sent)
-    #
-    #     if len(tokenized_sent) - 2 > self.max_seq_len:
-    #         # should not get here
-    #         print("Error: len(tokenized_sent) > self.max_seq_len")
-    #         tokenized_sent = tokenized_sent[:self.max_seq_len]
-    #
-    #     # Convert token to vocabulary indices
-    #     indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_sent)
-    #
-    #     segments_ids = [0] * len(indexed_tokens)
-    #
-    #     # Convert inputs to PyTorch tensors
-    #     tokens_tensor = torch.tensor([indexed_tokens])
-    #     segments_tensors = torch.tensor([segments_ids])
-    #
-    #     if self.to_cuda:
-    #         tokens_tensor = tokens_tensor.to('cuda')
-    #         segments_tensors = segments_tensors.to('cuda')
-    #
-    #     with torch.no_grad():
-    #         encoded_layers, _ = bert_pretrained(tokens_tensor, segments_tensors)
-    #
-    #     embbedings_per_token = self.mean_transform(encoded_layers)
-    #
-    #     masked_indices_set = set(masked_indices)
-    #     anti_mask_indices = [i for i in range(1, embbedings_per_token.shape[1] - 1) if i not in masked_indices_set]
-    #
-    #     embbedings_per_token_without_masked = self.remove_masked_embeddings(embbedings_per_token, anti_mask_indices)
-    #
-    #     embbedings_per_token_without_masked = embbedings_per_token_without_masked.squeeze(0)
-    #
-    #     embbedings_per_token_without_masked_paddded, paddings_mask, num_of_paddings = self.pad_embedded_sentence(
-    #         embbedings_per_token_without_masked)
-    #     anti_mask_indices += [-1] * num_of_paddings
-    #
-    #     embbedings_per_token_without_masked_paddded = self.concatenate_original_indices(
-    #         embbedings_per_token_without_masked_paddded, anti_mask_indices)
-    #
-    #     return embbedings_per_token_without_masked_paddded, paddings_mask, target_xs, target_ys
-    #
+
     def __len__(self):
         return len(self.data)
 
-    #
-    # def get_tokenizer(self):
-    #     return BertTokenizer.from_pretrained('bert-base-uncased')
 
     def pad_embedded_sentence(self, embedded_sent):
-        #  print("embedded_sent.shape:", embedded_sent.shape)
 
         num_of_paddings = self.max_seq_len - embedded_sent.shape[0]
         paddings = torch.zeros(num_of_paddings, dtype=torch.long)
@@ -154,6 +66,7 @@ class DatasetNonContextual(Dataset):
         if self.to_cuda:
             paddings_mask = paddings_mask.cuda()
         return padded_sent, paddings_mask, num_of_paddings
+
 
     def mask_sent(self, sent):
 
@@ -190,30 +103,3 @@ class DatasetNonContextual(Dataset):
         self.current_mask_ratio_index = (self.current_mask_ratio_index + 1) % len(self.mask_ratios)
 
         return sent, indices_to_mask, target_xs, target_ys, target_xs_mask
-
-        # def remove_masked_embeddings(self, sent_embedings, indices):
-        #     return sent_embedings[:, indices]
-        #
-        # def mean_transform(self, list_of_tensors):
-        #     big_tensor = torch.stack(list_of_tensors)
-        #     return big_tensor.mean(dim=0)
-
-        # def concatenate_original_indices(self, embbedings_per_token_without_masked, indices):
-        #     indices = torch.Tensor(indices).unsqueeze(1)
-        #     if self.to_cuda:
-        #         indices = indices.cuda()
-        #
-        #     return torch.cat((embbedings_per_token_without_masked, indices), 1)
-        #
-        # def update_xs_tokenized_indices(self, target_xs, tokenized_sent):
-        #     res = []
-        #     for idx, token in enumerate(tokenized_sent):
-        #         if token == '[MASK]':
-        #             res.append(idx)
-        #
-        #     for idx, x in enumerate(target_xs):
-        #         if x == -1:
-        #             res.extend(target_xs[idx:])
-        #             break
-        #
-        #     return res
