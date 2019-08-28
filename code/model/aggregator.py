3b741d5403b13c49f426da7c07950222a8f4fef0
Daniel Dubnikov <duvni@users.noreply.github.com>
2019-08-28 15:36:14 +0300

    refactor models

--- code/model/aggregator.py
+++ code/model/aggregator.py
@@ -4,7 +4,6 @@ import torch.nn.functional as F
 
 
 class AverageAggregator(nn.Module):
-    # def __init__(self, hidden_repr, to_cuda=False):
     def __init__(self):
         super(AverageAggregator, self).__init__()
 
@@ -14,11 +13,9 @@ class AverageAggregator(nn.Module):
         weights = F.softmax(weights.masked_fill(x_mask, float('-inf')), dim=1)
         weights = torch.unsqueeze(weights, dim=2)
 
-        # x = x.permute([0, 2, 1])  # transpose
         x = x.transpose(1, 2)
         x = torch.matmul(x, weights)  # batch matrix multiplication
         x = x.transpose(1, 2)
-        # x = x.permute([0, 2, 1])  # transpose
         
         return x
 
@@ -44,10 +41,8 @@ class AttentionAggregator(nn.Module):
         weights = F.softmax(energies, dim=1)
         weights = torch.unsqueeze(weights, dim=2)
 
-        # x = x.permute([0, 2, 1])  # transpose
         x = x.transpose(1, 2)
         x = torch.matmul(x, weights)  # batch matrix multiplication
         x = x.transpose(1, 2)
-        # x = x.permute([0, 2, 1])  # transpose
 
         return x
--- code/model/cross_attention_aggregator.py
+++ code/model/cross_attention_aggregator.py
@@ -1,22 +1,12 @@
 import torch.nn as nn
-# from model.multi_headed_attention import MultiHeadAttention
-from torch.nn import MultiheadAttention
-from model.aggregator import *
 
 
 class CrossAttentionAggregator(nn.Module):
-    """The Attention Aggregator module."""
-
     def __init__(self, embed_size, num_heads, dropout=0.1, to_cuda=False):
         super().__init__()
-        self.embed_size = embed_size
-        self.num_heads = num_heads
-        self.dropout = dropout
-        self.multihead_attention = MultiheadAttention(embed_dim=self.embed_size, num_heads=self.num_heads, dropout=self.dropout)
+        self.multihead_attention = nn.MultiheadAttention(embed_dim=embed_size, num_heads=num_heads, dropout=dropout)
         if to_cuda:
             self.multihead_attention = self.multihead_attention.cuda()
-        # self.aggregator = AttentionAggregator(embed_size, to_cuda)
-        # self.aggregator = AverageAggregator(embed_size, to_cuda)
 
     def forward(self, q, k, r, context_mask, target_mask):
         q = q.transpose(0, 1)
@@ -25,6 +15,4 @@ class CrossAttentionAggregator(nn.Module):
         rep = self.multihead_attention(q, k, r, context_mask)
         rep = rep[0].transpose(0, 1)
 
-        # rep = self.aggregator(rep, target_mask)
-        # rep = self.aggregator(r, context_mask)
         return rep
