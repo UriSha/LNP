066b28bebfea2bf0b373c57b16ceb0826e20bd0c
uri <urish92@gmail.com>
2019-08-31 23:33:12 +0300

    small bert change

--- code/bert_based/bert.py
+++ code/bert_based/bert.py
@@ -8,10 +8,14 @@ logging.basicConfig(level=logging.INFO)
 # Load pre-trained model tokenizer (vocabulary)
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 
-text = '[CLS] this book is such a great [MASK]. cant wait for the [MASK] one . [SEP]'
+text = '[CLS] this book is such a great [MASK] . cant wait for the [MASK] one . [SEP]'
 tokenized_text = tokenizer.tokenize(text)
 indexed_tokens = tokenizer.convert_tokens_to_ids(tokenized_text)
-masked_index = tokenized_text.index('[MASK]')
+#masked_index = tokenized_text.index('[MASK]')
+
+
+masked_index = 7
+masked_index2 = 13
 # Create the segments tensors.
 segments_ids = [0] * len(tokenized_text)
 
@@ -28,6 +32,11 @@ with torch.no_grad():
     predictions = model(tokens_tensor, segments_tensors)
 
 predicted_index = torch.argmax(predictions[0, masked_index]).item()
+predicted_token = tokenizer.convert_ids_to_tokens([predicted_index])[0]
+
+print(predicted_token)
+
+predicted_index = torch.argmax(predictions[0, masked_index2]).item()
 predicted_token = tokenizer.convert_ids_to_tokens([predicted_index])[0]
 
 print(predicted_token)
