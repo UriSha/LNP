f7c7b280617dad3e3ef93a4c8e8f7e0949bc37ad
Daniel Dubnikov <duvni@users.noreply.github.com>
2019-09-10 14:01:23 +0300

    final changes

--- README.md
+++ README.md
@@ -1,2 +1,23 @@
-# TICNP
-Conditional Neural Processes for Text infilling
+# Lingual Neural Processes
+This repository contains the code and data for our final project.
+
+### Prerequisites
+In order to train a model from scrach you need to have the following:
+- python 3.7
+- torch
+- numpy
+- skylearn
+- pytorch-pretrained-bert
+
+In addition, you need fasttext word embeddings file, get it [here](https://fasttext.cc/docs/en/english-vectors.html).
+- Download wiki-news-300d-1M.vec.zip
+- Unzip it and name the file fasttext.vec
+- locate fasttext.vec in data/embeddings/
+
+### Training the model
+```shell
+$ python main.py --data_file APRC.txt --mask_ratios 0.25 0.5 --learning_rate 0.0003 --test_size 0.1 --epochs 500 --to_cuda True
+```
+You can control and change the hyper-parameters to try different settings.
+
+In order to run without a gpu set the cuda parameter to False
--- code/main.py
+++ code/main.py
@@ -1,91 +1,101 @@
-import os
 import random
-from data_processing.dataset import DatasetNonContextual
-from data_processing.text_processor import TextProcessor
+import argumenter
+from logger import Logger
+from plotter import Plotter
 from model.cnp import CNP
 from training.trainer import Trainer
-from plotter import Plotter
-from logger import Logger
+from data_processing.dataset import DatasetNonContextual
+from data_processing.text_processor import TextProcessor
 
 
 def main():
-    to_cuda = False
-    train_mask_rations = [0.25, 0.5]
-    test_size = 0.5
-    topk = [1, 5, 10]
-    nheads = 2
-    use_weight_matrix = True
-    normalize_weights = True
-    use_latent = True
-    kl_weight = 1.0
-
-    dropout = 0
-    epoch_count = 20
-    random_every_time = True
-
     logger = Logger()
-    cur_dir = os.path.dirname(os.path.realpath(__file__))
+    logger.log("Starting CNP")
 
-    random.seed(a=5)
+    args = argumenter.parse_arguments(logger)
 
-    # text_processor = TextProcessor(os.path.join(cur_dir, "../data/APRC/APRC_new1.txt"),
-    #                                             os.path.join(cur_dir, "../data/embeddings/wiki-news-300d-1M.vec"), test_size=test_size, mask_ratio=mask_ratio,
-    #                                             sents_limit=10000, rare_word_threshold=1, use_weight_loss=True)
-    # text_processor = TextProcessor(os.path.join(cur_dir, "../data/APRC/APRC_small_mock.txt"),
-    #                                             os.path.join(cur_dir, "../data/embeddings/wiki-news-300d-1M.vec"), test_size=test_size, mask_ratio=mask_ratio,
-    #                                             sents_limit=10000, rare_word_threshold=1, use_weight_loss=True)
-    text_processor = TextProcessor(os.path.join(cur_dir, "../data/APRC/APRC_small_mock1.txt"),
-                                   os.path.join(cur_dir, "../data/embeddings/small_fasttext.txt"),
+    if abs(args.test_size - -1.0) < 0.01 and args.abs_test_size == -1:
+        raise Exception("At least one of test size parameters must be set")
+    if abs(args.test_size - -1.0) >= 0.01 and args.abs_test_size != -1:
+        raise Exception("Only one of test size parameters should be set")
+
+    test_size = args.abs_test_size
+    if args.abs_test_size == -1:
+        test_size = args.test_size
+
+    random_seed = args.random_seed
+    if not random_seed:
+        random_seed = random.randint(1, 2_000_000_000)
+    random.seed(a=random_seed)
+    logger.log(f"Using seed={random_seed}")
+
+    logger.log("Init Text Processor")
+    text_processor = TextProcessor("data/APRC/{}".format(args.data_file),
+                                   "data/embeddings/wiki-news-300d-1M.vec",
                                    test_size=test_size,
-                                   sents_limit=10000,
-                                   rare_word_threshold=0,
+                                   sents_limit=args.sent_count,
+                                   rare_word_threshold=args.rare_threshold,
                                    logger=logger)
 
-    train_dataset = DatasetNonContextual(text_processor.train_sents,
-                                         text_processor.max_seq_len,
-                                         mask_ratios=train_mask_rations,
-                                         random_every_time=random_every_time,
-                                         to_cuda=to_cuda)
-    eval_datasets = []
-    eval_datasets.append(DatasetNonContextual(text_processor.test25,
-                                        text_processor.max_seq_len,
-                                        mask_ratios=[0.25], to_cuda=to_cuda))
-    eval_datasets.append(DatasetNonContextual(text_processor.test50,
-                                        text_processor.max_seq_len,
-                                        mask_ratios=[0.5], to_cuda=to_cuda))
 
-    tags = [eval_ds.mask_ratios[0] for eval_ds in eval_datasets]
-    print("Vocab size: ", len(text_processor.id2w))
-    model = CNP(enc_hidden_layers=[512, 768],
-                dec_hidden_layers=[768, 1024, 512],
-                emb_weight = text_processor.embedding_matrix,
-                max_seq_len = text_processor.max_seq_len,
-                use_weight_matrix = use_weight_matrix,
-                dropout=dropout,
-                use_latent=use_latent,
-                nheads=nheads,
-                normalize_weights=normalize_weights,
-                to_cuda=to_cuda)
-    print("Model has {} parameters".format(sum(p.numel() for p in model.parameters() if p.requires_grad)))
-    # print(list(model.decoder.parameters()))
+    logger.log("Init Train Dataset")
+    train_dataset = DatasetNonContextual(sents=text_processor.train_sents,
+                                         max_seq_len=text_processor.max_seq_len,
+                                         mask_ratios=args.mask_ratios,
+                                         random_every_time=args.dataset_random_every_time,
+                                         to_cuda=args.to_cuda)
+
+    logger.log("Init Test Datasets")
+    test_datasets = []
+    test_datasets.append(DatasetNonContextual(sents=text_processor.test25,
+                                              max_seq_len=text_processor.max_seq_len,
+                                              mask_ratios=[.25],
+                                              to_cuda=args.to_cuda))
+
+    test_datasets.append(DatasetNonContextual(sents=text_processor.test50,
+                                              max_seq_len=text_processor.max_seq_len,
+                                              mask_ratios=[0.5],
+                                              to_cuda=args.to_cuda))
+
+    logger.log("Vocab size: ", len(text_processor.id2w))
+
+    logger.log("Init model")
+    model = CNP(enc_hidden_layers=args.enc_layers,
+                dec_hidden_layers=args.dec_layers,
+                emb_weight=text_processor.embedding_matrix,
+                max_seq_len=text_processor.max_seq_len,
+                use_weight_matrix=args.use_weight_matrix,
+                dropout=args.dropout,
+                use_latent=args.use_latent,
+                nheads=args.number_of_heads,
+                normalize_weights=args.normalize_weights,
+                to_cuda=args.to_cuda)
+
+    logger.log("Model has {} parameters".format(sum(p.numel() for p in model.parameters() if p.requires_grad)))
+    logger.log("Init Trainer")
+
+    # assume every test_ds has only one mask ration
+    tags = [test_ds.mask_ratios[0] for test_ds in test_datasets]
+
     trainer = Trainer(model=model,
                       train_dataset=train_dataset,
-                      test_datasets=eval_datasets,
+                      test_datasets=test_datasets,
                       tags=tags,
-                      batch_size=70,
-                      opt="ADAM",
-                      learning_rate=0.001,
-                      momentum=0.9,
-                      epoch_count=epoch_count,
-                      acc_topk=topk,
-                      kl_weight=kl_weight,
-                      print_interval=1,
+                      batch_size=args.batch_size,
+                      opt=args.opt,
+                      learning_rate=args.learning_rate,
+                      momentum=args.momentum,
+                      epoch_count=args.epochs,
+                      acc_topk=args.topk,
+                      kl_weight=args.kl_weight,
+                      print_interval=args.print_interval,
                       bleu_sents=text_processor.bleu_sents,
-                      to_cuda=to_cuda,
+                      to_cuda=args.to_cuda,
                       logger=logger,
                       id2w=text_processor.id2w)
-    train_loss, test_losses = trainer.run()
-    plotter = Plotter(train_loss, test_losses, tags, logger)
+    logger.log("Start training")
+    train_loss_per_epoch, test_losses_per_epoch = trainer.run()
+    plotter = Plotter(train_loss_per_epoch, test_losses_per_epoch, tags, logger)
     plotter.plot()
 
 
